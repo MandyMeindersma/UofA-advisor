6a3b0ca56465ef8f96a4701cc17c0e2b9d1f3525
Muhammad Umer Khan <um4r12@gmail.com>
2019-01-19 19:30:48 -0700

    adding models and mock data to the database

---
+++ backend/courses/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+
+class Faculty(models.Model):
+    faculty_name = models.CharField(max_length=128)
+class Department(models.Model):
+    department_name = models.CharField(max_length=128)
+class Program(models.Model):
+    program_name = models.CharField(max_length=128)
+
+class FacultyDepartment(models.Model):
+    faculty_id = models.ForeignKey(Faculty, on_delete=models.CASCADE, related_name = 'parent')
+    department_id = models.ForeignKey(Department, on_delete=models.CASCADE, related_name = 'child')
+
+    class Meta:
+        unique_together = ('faculty_id', 'department_id')
+class DepartmentProgram(models.Model):
+    department_id = models.ForeignKey(Department, on_delete=models.CASCADE, related_name = 'parent')
+    program_id = models.ForeignKey(Program, on_delete=models.CASCADE, related_name = 'child')
+
+    class Meta:
+        unique_together = ('department_id', 'program_id')
+# Create your models here.
+class Course(models.Model):
+    course_name = models.CharField(max_length=128)
+    course_num = models.IntegerField()
+    course_faculty = models.ForeignKey(Faculty, on_delete=models.CASCADE)
+    def __str__(self):
+        return "%s %s" % (self.course_name, self.course_num)
+    class Meta:
+        unique_together = ('course_name', 'course_num')
+       
+class CoursePrerequisite(models.Model):
+    course_id = models.ForeignKey(Course, on_delete=models.CASCADE, related_name = 'child')
+    course_prereq_id = models.ForeignKey(Course, on_delete=models.CASCADE, related_name = 'parent')
+    
+    class Meta:
+        unique_together = ('course_id', 'course_prereq_id')
+
+    
---
+++ backend/courses/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Course
+from .serializers import CourseSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+# Create your views here.
+
+@api_view(['get'])
+def fetch_courses(request):
+    #fetch all the course objects
+    courses = Course.objects.all()
+    #send the course objects as a response
+    serializer = CourseSerializer(courses, many=True)
+
+    return Response(serializer.data)
