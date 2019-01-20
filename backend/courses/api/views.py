6cb57d9c7fbe25729e2ab7fc84ae99cc02fcaa09
Ibrahim Aly <ialy@ualberta.ca>
2019-01-20 11:38:05 -0700

    added some functionality for query parameteres

--- backend/courses/api/views.py
+++ backend/courses/api/views.py
@@ -3,8 +3,18 @@ from courses.models import Course, Faculty, Department, Program
 from .serializers import CourseSerializer, FacultySerializer, DepartmentSerializer, ProgramSerializer
 
 class CourseListView(ListAPIView):
-    queryset = Course.objects.all()
+    queryset = Course.objects.all() 
     serializer_class = CourseSerializer
+   
+
+    def get_queryset(self):
+        queryset = Course.objects.all()
+
+        course_name = self.request.query_params.get('course_name', None)
+        if course_name is not None :
+            queryset = queryset.filter(course_name=course_name)
+
+        return queryset
 
 class CourseDetailView(RetrieveAPIView):
     queryset = Course.objects.all()
@@ -24,6 +34,27 @@ class DepartmentDetailView(RetrieveAPIView):
     
 class DepartmentListView(ListAPIView):
     queryset = Department.objects.all()
+
+    def get_queryset(self):
+        queryset = Department.objects.all()
+        print(self.request.query_params)
+        faculty_id = self.request.query_params.get('faculty_id', None)
+        if faculty_id is not None:
+            queryset = queryset.filter(faculty_id=faculty_id)
+        print(faculty_id)
+        return queryset
+    '''
+    def get_queryset(self):
+        """
+        This view should return a list of all the purchases
+        for the currently authenticated user.
+        """
+        queryset = Purchase.objects.all()
+        username = self.request.query_params.get('username', None)
+        if username is not None:
+            queryset = queryset.filter(purchaser__username=username)
+        return queryset
+    '''
     serializer_class = DepartmentSerializer
 
 class ProgramDetailView(RetrieveAPIView):
