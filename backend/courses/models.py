7942e880fad77224795f37a0eef163459d34c0c8
Ibrahim Aly <ialy@ualberta.ca>
2019-01-19 20:21:00 -0700

    added more models

--- backend/courses/models.py
+++ backend/courses/models.py
@@ -14,11 +14,11 @@ class FacultyDepartment(models.Model):
     class Meta:
         unique_together = ('faculty_id', 'department_id')
 class DepartmentProgram(models.Model):
-    department_id = models.ForeignKey(Department, on_delete=models.CASCADE, related_name = 'parent')
+    faculty_department_id = models.ForeignKey(FacultyDepartment, on_delete=models.CASCADE, related_name = 'parent')
     program_id = models.ForeignKey(Program, on_delete=models.CASCADE, related_name = 'child')
 
     class Meta:
-        unique_together = ('department_id', 'program_id')
+        unique_together = ('faculty_department_id', 'program_id')
 # Create your models here.
 class Course(models.Model):
     course_name = models.CharField(max_length=128)
@@ -36,4 +36,23 @@ class CoursePrerequisite(models.Model):
     class Meta:
         unique_together = ('course_id', 'course_prereq_id')
 
-    
+class RequiredCourse(models.Model):
+    department_program_id = models.ForeignKey(DepartmentProgram, on_delete=models.CASCADE,related_name="parent")
+    course_required = models.ForeignKey(Course, on_delete=models.CASCADE, related_name="child")
+
+    class Meta:
+        unique_together = ('department_program', 'course_required')
+
+class RequiredCredit(models.Model):
+    #e.g. biology specialization required credit
+    department_program_id = models.ForeignKey(DepartmentProgram, on_delete=models.CASCADE, related_name="parent")
+    #from the faculty
+    faculty_id = models.ForeignKey(Faculty, on_delete=models.CASCADE, related_name="child_faculty")
+    #from the department, if value is null pick anything
+    department_id = models.ForeignKey(Department on_delete=models.CASCADE, related_name="child_department",null=True)
+    credits = models.IntegerField()
+
+
+
+
+
